8a88ece2fd77a404cf793185b898a48537503add
SAM MESEL <sammesel@users.noreply.github.com>
2024-02-08 13:39:58 -0500

    Update 100_B5_Drop_User_Login.sql

--- 1 - Installation Configuration/100_B5_Drop_User_Login.sql
+++ 1 - Installation Configuration/100_B5_Drop_User_Login.sql
@@ -14,7 +14,11 @@ GO
 -- Drop LOGIN
 USE [master]
 GO
-DROP LOGIN [login_internal_principal_spconfigure]
+-- Add User to role
+ALTER ROLE role_internal_principal_spconfigure
+	DROP MEMBER user_internal_principal_spconfigure
 GO
 DROP ROLE [role_internal_principal_spconfigure]
+GO
+DROP LOGIN [login_internal_principal_spconfigure]
 GO
