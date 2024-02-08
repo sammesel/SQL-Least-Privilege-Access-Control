e5459c48867ce4678d05901f2aa1034325d79012
SAM MESEL <sammesel@users.noreply.github.com>
2024-02-08 13:32:51 -0500

    Update 100_B4_DROP_Objects.sql

--- 1 - Installation Configuration/100_B4_DROP_Objects.sql
+++ 1 - Installation Configuration/100_B4_DROP_Objects.sql
@@ -7,17 +7,17 @@ USE [master]
 GO
 
 
-/****** Object:  StoredProcedure [tools].[up_sp_configure]    Script Date: 12/21/2023 11:56:48 AM ******/
-DROP PROCEDURE IF EXISTS [tools].[up_sp_configure]
+/****** Object:  StoredProcedure [tools_sp_configure.].[up_sp_configure]    Script Date: 12/21/2023 11:56:48 AM ******/
+DROP PROCEDURE IF EXISTS [tools_sp_configure.].[up_sp_configure]
 GO
-DROP PROCEDURE IF EXISTS [tools].[up_sp_configure_option_value]
+DROP PROCEDURE IF EXISTS [tools_sp_configure.].[up_sp_configure_option_value]
 GO
-DROP PROCEDURE IF EXISTS [tools].[up_sp_configure_show_advanced_options]
+DROP PROCEDURE IF EXISTS [tools_sp_configure.].[up_sp_configure_show_advanced_options]
 GO
-DROP PROCEDURE IF EXISTS [tools].[up_sp_configure_advanced_option_value]
+DROP PROCEDURE IF EXISTS [tools_sp_configure.].[up_sp_configure_advanced_option_value]
 GO
-/****** Object:  Schema [tools]    Script Date: 12/21/2023 12:00:21 PM ******/
-DROP SCHEMA [tools]
+/****** Object:  Schema [tools_sp_configure.]    Script Date: 12/21/2023 12:00:21 PM ******/
+DROP SCHEMA [tools_sp_configure.]
 GO
 /****** Object:  User [internal_principal_spconfigure]    Script Date: 12/21/2023 11:54:15 AM ******/
 DROP USER [user_internal_principal_spconfigure]
