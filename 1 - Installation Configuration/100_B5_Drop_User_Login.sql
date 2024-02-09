fb779cb1eee5be0cff5fe1e1b87804ffa8baa20d
SAM MESEL <sammesel@users.noreply.github.com>
2024-02-08 20:40:41 -0500

    Update 100_B5_Drop_User_Login.sql

--- 1 - Installation Configuration/100_B5_Drop_User_Login.sql
+++ 1 - Installation Configuration/100_B5_Drop_User_Login.sql
@@ -1,15 +1,12 @@
 -- ================================================================================= --
 -- LOGIN as:		sa
--- use password:	'<password-place-holder>'
+-- use password:	'<P@ssw0rd-Pl@c3-H0ld3r>'
 -- ================================================================================= --
 SELECT USER_NAME(), SUSER_NAME() , session_user , system_user
 USE master
 GO
 SELECT USER_NAME(), SUSER_NAME() , session_user , system_user
 
--- Drop USER
-DROP USER [user_internal_principal_spconfigure]
-GO
 
 -- Drop LOGIN
 USE [master]
@@ -19,6 +16,9 @@ ALTER ROLE role_internal_principal_spconfigure
 	DROP MEMBER user_internal_principal_spconfigure
 GO
 DROP ROLE [role_internal_principal_spconfigure]
+GO
+-- Drop USER
+DROP USER [user_internal_principal_spconfigure]
 GO
 DROP LOGIN [login_internal_principal_spconfigure]
 GO
